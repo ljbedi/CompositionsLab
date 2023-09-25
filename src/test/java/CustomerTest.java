f37a59f0257e7446360c0af54032475f3d581691
John <ashton.basile@gmail.com>
2023-09-25 15:02:36 +0100

    I wrote the setup for the CustomerTest

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,27 @@
+import com.codebase.Customer;
+import com.codebase.Engine;
+import com.codebase.Vehicle;
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 public class CustomerTest {
+    private ArrayList<Vehicle> vehicles;
+    private Vehicle vehicle1;
+    private Vehicle vehicle2;
+    private Engine engine1;
+    private Engine engine2;
+    private Customer customer;
+
+    @Before
+    public void before() {
+        vehicle1 = new Vehicle(20_000, "Red", engine1);
+        vehicle2 = new Vehicle(10_000, "Blue", engine2);
+        vehicles = new ArrayList<>();
+        vehicles.add(vehicle1);
+        vehicles.add(vehicle2);
+        customer = new Customer(5_000, vehicles);
+    }
 }
