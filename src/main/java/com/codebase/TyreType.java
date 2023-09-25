27992ac9c953fc1167543f8c0b0e45d2f732589b
Liam Bailey <ljbailey17@gmail.com>
2023-09-25 15:21:13 +0100

    added enums

--- src/main/java/com/codebase/TyreType.java
+++ src/main/java/com/codebase/TyreType.java
@@ -1,4 +1,10 @@
 package com.codebase;
 
 public enum TyreType {
+
+    WINTER,
+    BUDGET,
+    OFFROAD,
+
+    RACING;
 }
--- src/main/java/com/codebase/VehicleType.java
+++ src/main/java/com/codebase/VehicleType.java
@@ -1,4 +1,20 @@
 package com.codebase;
 
 public enum VehicleType {
+
+    CAR(4),
+    MOTORBIKE(2),
+    TANK(6);
+
+    private int numTyres;
+
+    VehicleType(int numTyres) {
+        this.numTyres = numTyres;
+    }
+
+    public int getNumTyres() {
+        return numTyres;
+    }
+
+
 }
